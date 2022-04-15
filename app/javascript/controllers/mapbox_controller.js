89ccd56dd438e17e8c2eb52599a6c9df6bef1226
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-04-15 17:16:28 +0100

    added geocoder_controller to make sure when a flat is created there is the right address

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from "mapbox-gl"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 export default class extends Controller {
   static values = {
@@ -24,6 +25,9 @@ export default class extends Controller {
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(this.map)
     });
+
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }))
   }
 
   _fitMapToMarkers() {
