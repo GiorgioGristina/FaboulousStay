7bb450778681b4286b56de9cd6f2238f506a25bd
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-04-15 11:21:44 +0100

    generate model booking with check_out and check_in columns and totale_price, numbers_of_nights and foreign keys for user that represent the guest that do the reservation and flat foreign key that represent the flat that will be bookend and allow to acces the data of the user who rent that place. added controller and the routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_144356) do
+ActiveRecord::Schema.define(version: 2022_04_15_095533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "check_in"
+    t.date "check_out"
+    t.bigint "user_id", null: false
+    t.bigint "flat_id", null: false
+    t.integer "total_price"
+    t.integer "number_of_nights"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["flat_id"], name: "index_bookings_on_flat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -55,5 +68,7 @@ ActiveRecord::Schema.define(version: 2022_04_14_144356) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
   add_foreign_key "flats", "users"
 end
