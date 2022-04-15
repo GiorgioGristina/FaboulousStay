978b7d05371d25d6a9d73ccfb5a6dfad68f692a0
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-04-15 16:38:05 +0100

    add mapbox controller and add instance variable marker in the index to display all the marker on the map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_15_110259) do
+ActiveRecord::Schema.define(version: 2022_04_15_144356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2022_04_15_110259) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "home_type"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
